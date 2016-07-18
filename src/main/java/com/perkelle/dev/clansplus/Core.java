4373657e388340fb86893c62ea36d4c09dabe48c
snekman11 <officialgamecraftx@gmail.com>
2016-07-18 20:37:17 +0100

    Commit #3
    Finalized code for the night

--- src/main/java/com/perkelle/dev/clansplus/Core.java
+++ src/main/java/com/perkelle/dev/clansplus/Core.java
@@ -1,11 +1,16 @@
 package com.perkelle.dev.clansplus;
 
+import com.perkelle.dev.clansplus.commands.RegisterCmds;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
 
+    private RegisterCmds registerCmds;
+
     @Override
     public void onEnable() {
-
+        registerCmds = new RegisterCmds();
+        registerCmds.addCmds();
+        registerCmds.register(this);
     }
 }
