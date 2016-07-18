a265ccf3bd29fe90b9015885c715d6e911287ba4
snekman11 <officialgamecraftx@gmail.com>
2016-07-18 20:35:57 +0100

    Commit #2

--- src/main/java/com/perkelle/dev/clansplus/commands/RegisterCmds.java
+++ src/main/java/com/perkelle/dev/clansplus/commands/RegisterCmds.java
@@ -1,7 +1,10 @@
 package com.perkelle.dev.clansplus.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.Plugin;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,27 @@ public class RegisterCmds {
     }
 
     public void register(Plugin pl) {
-        //pl.getServer().g
+        //Credit to https://www.spigotmc.org/members/occanowey.1910/ and https://www.spigotmc.org/members/itssteve.8181/
+        CommandMap commandMap = null;
+        try {
+            final Field bukkitCommandMap = pl.getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+            commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+        } catch(NoSuchFieldException e) {
+            pl.getServer().getLogger().severe("Error #1 occurred - please report this to the author along with this:");
+            e.printStackTrace();
+        } catch(IllegalAccessException e) {
+            pl.getServer().getLogger().severe("Error #1 occurred - please report this to the author along with this:");
+            e.printStackTrace();
+        }
+
+        for(CCommand cmd : cmds) {
+            try {
+                commandMap.register(cmd.getName(), cmd);
+            } catch(NullPointerException e) {
+                pl.getServer().getLogger().severe("Error #2 occurred - please report this to the author along with this:");
+                e.printStackTrace();
+            }
+        }
     }
 }
